1aa26a0a1c3cbef861f4735fb6ad693400205f8b
dadazhu05 <111570050+dadazhu05@users.noreply.github.com>
2022-09-21 19:37:12 +0800

    init week
    init week

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts;
     console.log("fetching nfts");
-    const api_key = "2rBVHDFmWEY2GV4lT-APN7lVfMbCh5wF"
+    const api_key = "l-TwBY_QZnGT5eGADCJnaZyBDgi9GBGq"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
       method: 'GET'
